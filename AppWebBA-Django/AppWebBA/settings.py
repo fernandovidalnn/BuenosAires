8327430b40b070f35ad2246b1a77445487d311aa
Fernando Vidal <fe.vidaln@duocuc.cl>
2025-06-03 17:33:59 -0400

    Avance seguro antes de pasar a main

--- AppWebBA-Django/AppWebBA/settings.py
+++ AppWebBA-Django/AppWebBA/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'NAME': 'base_datos',
         'USER': 'sa',
         'PASSWORD': '123',
-        'HOST': 'LAPTOP-CJ7DC4L1',
+        'HOST': 'AOAWSB03LC0916',
         'PORT': '',
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
